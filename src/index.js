71bc1011dc14763a93802adc1150ec7082ac6937
Snehashis Banerjee <snehashis.banerjee@codeclouds.com>
2025-02-19 19:18:17 +0530

    Utils discussion

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 dotenv.config({ path: "./env" });
-connectDB();
-
-/*
-const app = express(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (err) => {
-      console.log("ERRR", err);
-      throw err;
-    });
-    app.listen(process.env.PORT, () => {
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
       console.log(`App is listening on PORT ${process.env.PORT}`);
     });
-  } catch (error) {
-    console.error(error);
-    throw err;
-  }
-})();
-*/
+    app.on("error", (error) => {
+      console.error("Server error", error);
+      process.exit(1);
+    });
+  })
+  .catch((err) => {
+    console.error("MONGODB connection failed", err);
+  });
