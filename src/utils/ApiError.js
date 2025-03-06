e2be9df277f972ab6efe31324c915d98e85ccfeb
Snehashis Banerjee <snehashis.banerjee@codeclouds.com>
2025-03-06 13:11:25 +0530

    jwt setup

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,16 +1,17 @@
 class ApiError extends Error {
-    constructor(
-        statusCode,
-        message="Something went wrong",
-        errors=[],
-        stack=""
-    ) {
-        super(message)
-        this.statusCode=statusCode,
-        this.data=null,
-        this.message=message
-        this.success=false
-        this.errors=errors
-    }
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    (this.statusCode = statusCode),
+      (this.data = null),
+      (this.message = message);
+    this.success = false;
+    this.errors = errors;
+  }
 }
-export default ApiError
+
+export default ApiError;
